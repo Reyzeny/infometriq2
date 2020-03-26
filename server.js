351ece565781f62ed4a4d7f56196aa050952259b
pelumzeny <pelumzeny10@gmail.com>
2020-03-26 20:10:32 +0100

    general css not affecting other views

--- server.js
+++ server.js
@@ -10,14 +10,14 @@
 //   console.log('listening on port ', server.address().port);
 // });
 
-// var history = require('connect-history-api-fallback');
+var history = require('connect-history-api-fallback');
 
 var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 app = express()
 //add this middleware
-// app.use(history());    
+app.use(history());    
 app.use(express.static(path.join(__dirname, 'dist')));
 var port = process.env.PORT || 5000
 app.listen(port)
